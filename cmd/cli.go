b4cdd98fb9843021ea41b481692ea068fa7bd59f
Victor Ferreira Teixeira da Silva <victor.silva@grinply.com>
2024-03-01 01:29:39 -0300

    feat: separate json print for quake games in a different function

--- cmd/cli.go
+++ cmd/cli.go
@@ -18,30 +18,9 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		file := args[0]
 		gameID, _ := cmd.Flags().GetInt("game")
-		groupedGames := parser.ParserQuakeGameFile(file, gameID)
-
-		keys := make([]int, 0)
-		for k := range groupedGames.GameDetails {
-			keys = append(keys, k)
-		}
-
-		sort.Ints(keys)
-
-		for _, gameID := range keys {
-			game := groupedGames.GameDetails[gameID]
-			gameMap := map[string]*parser.Game{fmt.Sprintf("game_%d", gameID): game}
-			gameJSON, err := json.MarshalIndent(gameMap, "", "  ")
-			if err != nil {
-				log.Fatalf("Error marshaling game %d: %v", gameID, err)
-			}
-			fmt.Println(string(gameJSON))
-			fmt.Println()
-		}
+		quakeGames := parser.ParserQuakeGameFile(file, gameID)
+		printJsonQuakeGames(quakeGames)
 	},
-}
-
-func init() {
-	rootCmd.PersistentFlags().Int("game", -1, "Specify the game ID to summarize")
 }
 
 // Execute executes the root command.
@@ -49,5 +28,29 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().Int("game", -1, "Specify the game ID to summarize")
+}
+
+func printJsonQuakeGames(quakeGames *parser.QuakeGames) {
+	keys := make([]int, 0)
+	for k := range quakeGames.GameDetails {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	for _, gameID := range keys {
+		game := quakeGames.GameDetails[gameID]
+		gameMap := map[string]*parser.Game{fmt.Sprintf("game_%d", gameID): game}
+		gameJSON, err := json.MarshalIndent(gameMap, "", "  ")
+		if err != nil {
+			log.Fatalf("Error marshaling game %d: %v", gameID, err)
+		}
+		fmt.Println(string(gameJSON))
+		fmt.Println()
 	}
 }
